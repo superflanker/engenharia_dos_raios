4f82b07ea1e0fb2d127a593dec7a7966c7fdacb6
Augusto Mathias Adams <superflankerCTBA@gmail.com>
2023-05-14 17:16:06 -0300

    Primeira Parcial Terminada

--- compile.sh
+++ compile.sh
@@ -74,9 +74,36 @@ cd ../aula_19
 pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
 cp one_minute_paper.pdf ../Portfolio/aula_19.pdf
 
+cd ../aula_20
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_20.pdf
+
+cd ../aula_21
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_21.pdf
+
+cd ../atividade_remota_video_01
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/atividade_remota_video_01.pdf
+
+cd ../atividade_remota_video_02
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/atividade_remota_video_02.pdf
+
+cd ../atividade_remota_video_03
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/atividade_remota_video_03.pdf
+
+cd ../atividade_remota_video_04
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/atividade_remota_video_04.pdf
+
 cd ../Trabalho_Primeira_Parcial
 pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "descargas_negativas_descendentes".tex
 cp descargas_negativas_descendentes.pdf ../Portfolio/Trabalho_Primeira_Parcial.pdf
+
+cd ../Portfolio
+pdfunite atividade_remota_video_01.pdf atividade_remota_video_02.pdf atividade_remota_video_03.pdf atividade_remota_video_04.pdf aula_01.pdf aula_03.pdf aula_04.pdf aula_05.pdf aula_06.pdf aula_07.pdf aula_08.pdf aula_09.pdf aula_10.pdf aula_11.pdf aula_12.pdf aula_13.pdf aula_14.pdf aula_15.pdf aula_16.pdf aula_17.pdf aula_18.pdf aula_19.pdf aula_20.pdf aula_21.pdf Trabalho_Primeira_Parcial.pdf primeira_parcial.pdf
 
 
 
