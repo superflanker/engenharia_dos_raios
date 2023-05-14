211cd7044a4b02869e00267e96ec5426c5c316db
Augusto Mathias Adams <superflankerCTBA@gmail.com>
2023-05-13 23:59:01 -0300

    Primeira Parcial quase Fechada

---
+++ compile.sh
@@ -0,0 +1,85 @@
+cd aula_01/
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_01.pdf
+
+cd ../aula_02
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_02.pdf
+
+cd ../aula_03
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_03.pdf
+
+cd ../aula_04
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_04.pdf
+
+cd ../aula_05
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_05.pdf
+
+cd ../aula_06
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_06.pdf
+
+cd ../aula_07
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_07.pdf
+
+cd ../aula_08
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_08.pdf
+
+cd ../aula_09
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_09.pdf
+
+cd ../aula_10
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_10.pdf
+
+cd ../aula_11
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_11.pdf
+
+cd ../aula_12
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_12.pdf
+
+cd ../aula_13
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_13.pdf
+
+cd ../aula_14
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_14.pdf
+
+cd ../aula_15
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_15.pdf
+
+cd ../aula_16
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_16.pdf
+
+cd ../aula_17
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_17.pdf
+
+cd ../aula_18
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_18.pdf
+
+cd ../aula_19
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "one_minute_paper".tex
+cp one_minute_paper.pdf ../Portfolio/aula_19.pdf
+
+cd ../Trabalho_Primeira_Parcial
+pdflatex -synctex=1 -interaction=nonstopmode  --shell-escape "descargas_negativas_descendentes".tex
+cp descargas_negativas_descendentes.pdf ../Portfolio/Trabalho_Primeira_Parcial.pdf
+
+
+
+
+
+
